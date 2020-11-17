dcd643c3a347e93ca0704de6eebd4464d52039ef
TrackDR <jeremydjackson@gmail.com>
2020-11-16 20:17:39 -0500

    Update generatebibleverseimage.py

--- generatebibleverseimage.py
+++ generatebibleverseimage.py
@@ -18,7 +18,7 @@ instaheight = 1080
 videowidth = 1920
 videoheight = 1080
 
-filename = "Wisdom13-15-insta.jpg"
+filename = "Wisdom14-3-insta.jpg"
 imgwidth = instawidth
 imgheight = instaheight
 
@@ -27,30 +27,25 @@ canvas = ImageDraw.Draw(img)
 
 font = ImageFont.truetype('arial.ttf', size=80)
 
-#text_width, text_height = canvas.textsize('Hello World', font=font)
-
-text = "Wisdom 13:10. But unhappy are they, and their hope is among the dead, who have \
-called gods the works of the hand of men, gold and silver, the \
-inventions of art, and the resemblances of beasts, or an unprofitable \
-stone the work of an ancient hand."
-
-text = "Wisdom 13:11. Or if an artist, a carpenter, hath cut down a tree proper for his \
-use in the wood, and skilfully taken off all the bark thereof, and with \
-his art, diligently formeth a vessel profitable for the common uses of \
-life," 
-
-text = "Wisdom 13:12. And useth the chips of his work to dress his meat:"
-
-text = "Wisdom 13:13. And taking what was left thereof, which is good for nothing, \
-being a crooked piece of wood, and full of knots, carveth it diligently \
-when he hath nothing else to do, and by the skill of his art fashioneth \
-it, and maketh it like the image of a man:"
-
-#text = "Wisdom 13:14. Or the resemblance of some beast, laying it over with vermilion, \
-#and painting it red, and covering every spot that is in it:"
-
 text = "Wisdom 13:15. And maketh a convenient dwelling place for it, and setting it in \
 a wall, and fastening it with iron,"
+
+text = "Wisdom 13:17. And then maketh prayer to it, enquiring concerning his substance, \
+and his children, or his marriage.  And he is not ashamed to speak to \
+that which hath no life:"
+
+text = "Wisdom 13:18. And for health he maketh supplication to the weak, and for life \
+prayeth to that which is dead, and for help calleth upon that which is \
+unprofitable:"
+
+text = "Wisdom 14:1. Again, another designing to sail, and beginning to make his voyage \
+through the raging waves, calleth upon a piece of wood more frail than \
+the wood that carrieth him."
+
+text = "Wisdom 14:2. For this the desire of gain devised, and the workman built it by his skill."
+
+text = "Wisdom 14:3. But thy providence, O Father, governeth it: for thou hast made a \
+way even in the sea, and a most sure path among the waves,"
 
 instanumchars = 28
 videonumchars = 40
